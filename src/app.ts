0e921b6b7990143ce4474288871e1e3703abb08b
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-03-23 01:18:13 +0100

    Fix everything

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+
+/**
+ * API examples routes.
+ */
+app.get("/api", () => "");
+
+
+export default app;
