f0c855c0311f4212eccee79c50f26d90488cc5e2
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-04-02 04:19:48 +0200

    news urls

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import express from "express";
 import { getFullStories, getWebsiteData, getWebsiteDataBySearch } from "./controllers/website";
 
@@ -33,6 +34,30 @@ app.get("/stories", getFullStories);
 
 app.get("/search", getWebsiteDataBySearch);
 
+const getVQD = async (query: string) => {
+  const vqd = await axios.get(`https://duckduckgo.com/?q=${query}&ia=web`).then(res => {
+    return res.data.match(/vqd=([0-9]+-[0-9]+-[0-9]+)/gm)[0].split("=")[1];
+  });
+
+  return vqd;
+};
+
+
+app.get("/search/videos", async (request, response)  => {
+  const query = request.query.searchQuery;
+  const vqd = await getVQD(query as string);
+  const data = await axios.get(`https://duckduckgo.com/v.js?l=us-en&o=json&q=${query}&vqd=${vqd}`);
+
+  response.send(data.data.results);
+});
+
+app.get("/search/news", async (request, response)  => {
+  const query = request.query.searchQuery;
+  const vqd = await getVQD(query as string);
+  const data = await axios.get(`https://duckduckgo.com/news.js?l=us-en&o=json&q=${query}&vqd=${vqd}`);
+
+  response.send(data.data.results);
+});
 
 
 export default app;
